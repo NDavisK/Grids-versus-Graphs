3ab2b1d8a5428eba0d6c2c3a4c0bdca1a4480dcb
NDavisK <35481054+NDavisK@users.noreply.github.com>
2020-04-16 10:11:28 +0530

    Update Models.py

--- GraphLSTM/Models.py
+++ GraphLSTM/Models.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  2 23:38:57 2018
-
-@author: zhiyong
-"""
-
-
 import torch.nn.functional as F
 import torch
 import torch.nn as nn
@@ -14,79 +6,7 @@ from torch.nn.parameter import Parameter
 from Modules import FilterLinear
 import math
 import numpy as np
-
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-        #print("RNN_init_")
-        self.hidden_size = hidden_size
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        #print("in RNN-forward")
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        return output, hidden
-    def initHidden(self, batch_size):
-        #print("in RNN-initHidden")
-        use_gpu = torch.cuda.is_available()
-        if use_gpu:
-            return Variable(torch.zeros(batch_size, self.hidden_size).cuda())
-        else:
-            return Variable(torch.zeros(batch_size, self.hidden_size))
-        
-        
-class LSTM(nn.Module):
-    def __init__(self, input_size, cell_size, hidden_size):
-        """
-        cell_size is the size of cell_state.
-        hidden_size is the size of hidden_state, or say the output_state of each step
-        """
-        super(LSTM, self).__init__()
-        print("LSTM_init_")
-        self.cell_size = cell_size
-        self.hidden_size = hidden_size
-        self.fl = nn.Linear(input_size + hidden_size, hidden_size)
-        self.il = nn.Linear(input_size + hidden_size, hidden_size)
-        self.ol = nn.Linear(input_size + hidden_size, hidden_size)
-        self.Cl = nn.Linear(input_size + hidden_size, hidden_size)
-        
-    def forward(self, input, Hidden_State, Cell_State):
-        combined = torch.cat((input, Hidden_State), 1)
-        print("in LSTM-forward")
-        f = F.sigmoid(self.fl(combined))
-        i = F.sigmoid(self.il(combined))
-        o = F.sigmoid(self.ol(combined))
-        C = F.tanh(self.Cl(combined))
-        Cell_State = f * Cell_State + i * C
-        Hidden_State = o * F.tanh(Cell_State)
-        
-        return Hidden_State, Cell_State
-    
-    def loop(self, inputs):
-        print("in LSTM-loop")
-        batch_size = inputs.size(0)
-        time_step = inputs.size(1)
-        Hidden_State, Cell_State = self.initHidden(batch_size)
-        for i in range(time_step):
-            Hidden_State, Cell_State = self.forward(torch.squeeze(inputs[:,i:i+1,:]), Hidden_State, Cell_State)  
-        return Hidden_State, Cell_State
-    
-    def initHidden(self, batch_size):
-        print("in LSTM-initHidden")
-        use_gpu = torch.cuda.is_available()
-        if use_gpu:
-            Hidden_State = Variable(torch.zeros(batch_size, self.hidden_size).cuda())
-            Cell_State = Variable(torch.zeros(batch_size, self.hidden_size).cuda())
-            return Hidden_State, Cell_State
-        else:
-            Hidden_State = Variable(torch.zeros(batch_size, self.hidden_size))
-            Cell_State = Variable(torch.zeros(batch_size, self.hidden_size))
-            return Hidden_State, Cell_State
-        
-        
+      
 class GraphConvolutionalLSTM(nn.Module):
     
     def __init__(self, K, A, FFR, feature_size, Clamp_A=True):
